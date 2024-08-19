a2053487417bb58935778638e36db1ca548f4479
Fabian Regul <yustif30@gmail.com>
2024-08-18 22:33:46 -0500

    colocando la logica dentro de la funcion suma

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const caja_uno = parseInt(document.getElementById("caja_uno").value);
-const caja_dos = parseInt(document.getElementById("caja_dos").value);
-const resultado = document.getElementById("resultado");
+function suma() {
+    const caja_uno = parseInt(document.getElementById("caja_uno").value);
+    const caja_dos = parseInt(document.getElementById("caja_dos").value);
+    const resultado = document.getElementById("resultado");
 
-let operacion = caja_uno + caja_dos;
-resultado.innerText = `el resultado es: ${operacion}`;
+    let operacion = caja_uno + caja_dos;
+    resultado.innerText = `el resultado es: ${operacion}`;
+}

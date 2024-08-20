89ecd03416a44330da7b10fb9287dce01289757f
Fabian Regul <yustif30@gmail.com>
2024-08-19 22:09:35 -0500

    modificacion del html,css y js, agregamos img

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function suma() {
-    const caja_uno = parseInt(document.getElementById("caja_uno").value);
-    const caja_dos = parseInt(document.getElementById("caja_dos").value);
-    const resultado = document.getElementById("resultado");
+//NOTA: Siempre documentar, comentar el codigo
 
-    let operacion = caja_uno + caja_dos;
+function suma() {
+
+    //1 - declaracion y parseo de variables   
+    const num_uno = parseInt(document.getElementById("num_uno").value);
+    const num_dos = parseInt(document.getElementById("num_dos").value);
+    const resultado = document.getElementById("resultado");
+    
+    //2 - verificacion - campos vacios
+    if (isNaN(num_uno) || isNaN(num_dos)) {
+            alert("Por favor, ingresa solo números.");
+        }
+    
+    //3 - operacion
+    let operacion = num_uno + num_dos;
+    
+    //4 - imprimiendo resultado con plantillas
     resultado.innerText = `el resultado es: ${operacion}`;
-}
+    
+    
+    }

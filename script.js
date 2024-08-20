2511db1c729c2057c411bbcc2b6871528a85b935
Fabian Regul <yustif30@gmail.com>
2024-08-19 22:14:57 -0500

    agregando nueva funcionalidad en js y html: tabla

--- script.js
+++ script.js
@@ -18,5 +18,19 @@ function suma() {
     //4 - imprimiendo resultado con plantillas
     resultado.innerText = `el resultado es: ${operacion}`;
     
+    //5 - tabla
+    // Agregar el resultado a la tabla
+    const tablaResultados = document.getElementById("tablaResultados").getElementsByTagName('tbody')[0];
+    const nuevaFila = tablaResultados.insertRow();
+
+    // Insertar celdas en la nueva fila
+    let celdaNumUno = nuevaFila.insertCell(0);
+    let celdaNumDos = nuevaFila.insertCell(1);
+    let celdaResultado = nuevaFila.insertCell(2);
+
+    // Agregar el contenido a las celdas
+    celdaNumUno.innerText = num_uno;
+    celdaNumDos.innerText = num_dos;
+    celdaResultado.innerText = operacion;
     
     }
